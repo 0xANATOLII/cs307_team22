c21324af4e3e841c08a82a180efece3f8c53d5ab
JuanOlivencia <jolivenc@purdue.edu>
2024-09-30 15:17:46 -0400

    login logic complete

--- client/Screens/HomePage.js
+++ client/Screens/HomePage.js
@@ -6,10 +6,39 @@ import styles from '../styles';
 export default function HomePage({ navigation }) {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
 
-  const handleLogin = () => {
+  const handleLogin = async () => {
     // Basic form validation can go here
     console.log("Logging in with:", username, password);
+    try {
+      const response = await fetch('http://localhost:4000/api/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          username: username,
+          password: password,
+        }),
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        console.log(data)
+        if (data.status === 'error') {
+          setErrorMessage(data.message);
+        } else {
+          setErrorMessage('');
+          //nav to dash board
+        }
+        
+      }
+    } catch (error) {
+      console.error('error logging in', error);
+      setErrorMessage('Something went wrong. Please try again.');
+    }
   };
 
   return (
@@ -32,6 +61,7 @@ export default function HomePage({ navigation }) {
         onChangeText={setPassword}
         secureTextEntry
       />
+      {errorMessage ? <Text style={styles.errorText}>{errorMessage}</Text> : null}
       <View style={styles.button}>
       <Pressable 
         onPress={handleLogin}
--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  app.enableCors({
+    origin: 'http://localhost:8081', // Allow requests from your frontend origin
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true, // Allow credentials like cookies (if needed)
+  });
+
   await app.listen(4000);
 }
 bootstrap();
