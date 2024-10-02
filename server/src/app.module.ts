ef845e6f26d4434e0b46ed7ffe78c486268a8518
Anatolii <anatoly.postelnik@gmail.com>
2024-10-02 14:31:13 -0400

    Mongo

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal:true, //This gets our .env file to make it global
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory:(configService: ConfigService) =>({
+        uri: configService.get<string>('MONGO_CONNECT'),
+      }),
+    }),
+  ],
+
+
+
   controllers: [AppController],
   providers: [AppService],
 })
--- server/src/main.ts
+++ server/src/main.ts
@@ -2,7 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  const port = process.env.PORT || 3000; // You can change the port here
+
+  await app.listen(port);
+  
+  // Ensure proper cleanup when the app is stopped
+  process.on('SIGINT', async () => {
+    await app.close();
+    process.exit(0);
+  });
+
 }
 bootstrap();
