cc3a65a6d6c857669c90cb45ac6c48839a73df23
pranavperumal <pranavpperumal@gmail.com>
2024-10-29 23:52:23 -0400

    Added all Nearby Monument Functionality

--- client/config.js
+++ client/config.js
@@ -1,5 +1,5 @@
 const Config = {
-    API_URL: 'https://4f86-128-210-106-58.ngrok-free.app',
+    API_URL: 'https://c198-2601-803-8180-b8d0-70da-c164-493c-78ee.ngrok-free.app',
     // You can add other global variables here if needed
   };
   
