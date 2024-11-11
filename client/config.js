85f1d44fbcd87cf5403ef5c870e8efb8a72fccf6
Ateeq Ramlan <aahmedra@purdue.edu>
2024-11-10 19:43:24 -0500

    swiping bare bones implementation, need to make navigation and new page

--- client/config.js
+++ client/config.js
@@ -1,5 +1,5 @@
 const Config = {
-    API_URL: 'https://098a-2601-803-8180-b8d0-ccf8-d876-b019-5b9f.ngrok-free.app',
+    API_URL: 'https://ddee-128-210-106-51.ngrok-free.app',
     // You can add other global variables here if needed
   };
   
