2cd7d2b6866741a22d7b9a77472dbf4f643b8f83
pranavperumal <pranavpperumal@gmail.com>
2024-11-16 13:38:26 -0500

    expo update

--- client/config.js
+++ client/config.js
@@ -1,5 +1,5 @@
 const Config = {
-    API_URL: 'https://ae32-2601-803-8180-b8d0-ccf8-d876-b019-5b9f.ngrok-free.app',
+    API_URL: 'https://db9a-128-210-106-59.ngrok-free.app',
     // You can add other global variables here if needed
   };
   
