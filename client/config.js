f9b9fbd09627dbd99a6107ba6ca10fb5ef6cb39f
pranavperumal <pranavpperumal@gmail.com>
2024-11-17 21:47:41 -0500

    Update FriendsScreen

--- client/config.js
+++ client/config.js
@@ -1,5 +1,5 @@
 const Config = {
-    API_URL: 'https://ddee-128-210-106-51.ngrok-free.app',
+    API_URL: 'https://564a-2a09-bac1-76c1-4960-00-e-31f.ngrok-free.app',
     // You can add other global variables here if needed
   };
   
